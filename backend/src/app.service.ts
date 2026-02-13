8d0bd82fc4fa5f9d1ab9d876951d026b199ab6a3
Ramesh A <ramesha@buckman.com>
2026-02-13 14:47:14 +0530

    Update API root message for clarity

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'WealthWatch API is Running! Please visit the Frontend URL to login.';
   }
 }
